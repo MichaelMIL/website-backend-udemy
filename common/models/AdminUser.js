f79ce82927e2736ce0a65c38bdec3256aaf49a4a
MichaelMIL <michaelmark16@gmail.com>
2020-10-23 21:58:17 +0300

    fixed issue with setting admin
    ACL to user isnt working

--- common/models/AdminUser.js
+++ common/models/AdminUser.js
@@ -8,21 +8,29 @@ var app = require('../../server/server');
 
 module.exports = function(AdminUser) {
     AdminUser.newAdmin = function(user_id){
-        console.log("trieng to create")
             app.models.AdminUser.find({where:{id: user_id}}, (err, users)=> {
-                console.log("user",err,users);
+              if(!err&&users){
                 app.models.Role.find({where:{name: 'admin'}}, (err2, role)=>{
-                  console.log("role",err2, role);
-                  app.models.RoleMapping.create({
-                    principalType: app.models.RoleMapping.USER,
-                    principalId: users[0].id,
-                    roleId: role[0].id
-                  }, (err3, principal)=>{
-                    console.log("done", err3, principal)
-                  })
+                  if(!err2&& role){
+                    app.models.RoleMapping.create({
+                      principalType: app.models.RoleMapping.USER,
+                      principalId: users[0].id,
+                      roleId: role[0].id
+                    }, (err3, principal)=>{
+                      if(!err3,principal){
+                        console.log("admin set", principal)
+                      }else{
+                        console.log("cant create proncipal", err3)
+                      }
+                    })
+                  }else{
+                    console.log("cant find admin role", err2)
+                  }
                 })
-              });
-            
+              }else{
+                console.log("cant find admin user", err);
+              }
+              });           
         }
     
     AdminUser.remoteMethod('newAdmin',
