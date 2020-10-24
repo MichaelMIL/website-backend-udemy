c01670b4e473f9ec54a4efc7c43a22995173fa00
MichaelMIL <michaelmark16@gmail.com>
2020-10-24 18:38:19 +0300

    retiered admin user

--- common/models/user.js
+++ common/models/user.js
@@ -1,7 +1,52 @@
 'use strict';
+// const loopback = require('loopback');
+// const { Role, Principal } = require('loopback');
 
-module.exports = function(User) {
+var app = require('../../server/server');
+
+//const app = module.exports = loopback();
+
+module.exports = function(user) {
+    user.newAdmin = function(user_id){
+            app.models.user.find({where:{id: user_id}}, (err, users)=> {
+              if(!err&&users){
+                app.models.Role.find({where:{name: 'admin'}}, (err2, role)=>{
+                  if(!err2&& role){
+                    app.models.RoleMapping.create({
+                      principalType: app.models.RoleMapping.USER,
+                      principalId: users[0].id,
+                      roleId: role[0].id
+                    }, (err3, principal)=>{
+                      if(!err3,principal){
+                        console.log("admin set", principal)
+                      }else{
+                        console.log("cant create proncipal", err3)
+                      }
+                    })
+                  }else{
+                    console.log("cant find admin role", err2)
+                  }
+                })
+              }else{
+                console.log("cant find admin user", err);
+              }
+              });           
+        }
+    
+    user.remoteMethod('newAdmin',
+    {
+        description: 'Create admin user',
+        accepts:[
+            {arg: 'user_id',type: 'string'},
+        ],
+
+    })
+
+
+
+
 
 };
+
 
 
